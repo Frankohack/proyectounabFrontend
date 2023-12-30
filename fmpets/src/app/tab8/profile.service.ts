f190594b36ee9d9fb075fa30c74b719f0148ca4c
francomolina <francomolina1997.fm@gmail.com>
2023-12-30 18:00:57 -0300

    paraBastian

--- fmpets/src/app/tab8/profile.service.ts
+++ fmpets/src/app/tab8/profile.service.ts
@@ -8,12 +8,10 @@ export class ProfileService {
   private userProfileData: BehaviorSubject<any> = new BehaviorSubject<any>(null);
 
   constructor() {
-    // Puedes inicializar datos del perfil aquí o obtenerlos del almacenamiento local o del backend.
     this.userProfileData.next({
       name: 'Usuario Ejemplo',
       email: 'usuario@example.com',
       phone: '123456789',
-      // Otros campos...
     });
   }
 
@@ -22,7 +20,7 @@ export class ProfileService {
   }
 
   updateUserProfile(profileData: any) {
-    // Actualiza el perfil del usuario, podría incluir una llamada al backend.
+
     this.userProfileData.next(profileData);
   }
 }
